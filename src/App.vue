fffc4b61aa532309de5d6a174b573b2864e6e41e
Jose Jaime Come <josejaimecome@gmail.com>
2023-03-06 19:00:36 +0200

    inputs and functions added

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <script setup>
-import HomeView from './views/HomeView.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import {reactive, ref, watch } from 'vue'
+
 </script>
 
 <template>
     <div class="wrapper">
-      <HomeView />
+      <Header text="Vue Script Setup App" />
+      <div>
+          <h2>Student Enrollment</h2>
+          <input type="text" v-model="student.firstname" placeholder="Student First Name" />
+          <input type="text" v-model="student.lastname" placeholder="Student Last Name" />
+          <input type="number" v-model="student.age" placeholder="Student Age" />
+      </div>
+      <Footer />
     </div>
 </template>
---
+++ src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+      2023
+    </div>
+  </template>
