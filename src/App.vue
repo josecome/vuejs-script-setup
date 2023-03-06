f5a0d31bae62be56b9e23a1c59873c7d2890c836
Jose Jaime Come <josejaimecome@gmail.com>
2023-03-06 20:30:02 +0200

    Minor changes made

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <script setup>
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
-import {reactive, ref, watch } from 'vue'
 
-import { reactive, ref, watch } from "vue";
+import { reactive, ref, computed, watch } from "vue";
 
-const student = reactive({ firstname: "", lastname: "", birthyear });
+const student = reactive({ firstname: "", lastname: "", birthyear: 1900 });
 const age = ref(0);
 
 const calculatedAge = () => {
   age.value = (new Date().getFullYear()) - student.birthyear;
 };
 
-
+const fullname = computed(() => "" + student.firstname + " " + student.lastname)
 
 watch(student, (val, oldval) => {
   console.log(val, oldval);
@@ -27,8 +26,9 @@ watch(student, (val, oldval) => {
           <h2>Student Enrollment</h2>
           <input type="text" v-model="student.firstname" placeholder="Student First Name" />
           <input type="text" v-model="student.lastname" placeholder="Student Last Name" />
-          <input type="number" v-model="student.birthyear" placeholder="Birth Year" />
-          <label {{ age }} />
+          <input type="number" v-model="student.birthyear" placeholder="Birth Year" min="1900" /><br />
+          FullName: {{ fullname }}<br />
+          Age: {{ age }}
       </div>
       <Footer />
     </div>
